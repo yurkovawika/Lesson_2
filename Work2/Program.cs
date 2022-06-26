311a1b3f254aa1bfbbcd4972ade4423371421fae
Viktorinkiss <yurkovawika@gmail.com>
2022-06-26 21:12:09 +0500

    Creat Work2

---
+++ Work2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите  число  > ");
+string array = Console.ReadLine();
+int length = array.Length;  //длина строки.
+
+if (length < 3)
+{
+   Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+   Console.WriteLine($"Третья цифра -> {array[2]}");
+}
